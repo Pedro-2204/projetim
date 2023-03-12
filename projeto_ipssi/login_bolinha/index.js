34ade7cf0a3877720a2c86292ee054e96356bf51
Pedro Rodrigues <106970471+Pedro-2204@users.noreply.github.com>
2023-03-11 21:20:09 -0300

    Add files via upload

---
+++ projeto_ipssi/login_bolinha/index.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    checkInputs()
+})
+
+function checkInputs(){
+    const usernameValue = username.value.trim()
+    const emailValue = email.value.trim()
+
+    if (usernameValue === ''){
+        errorValidation(username, 'Preencha esse campo')
+    }else{
+        successValidation(username)
+    }
+
+    if(emailValue === ''){
+        errorValidation(email, 'Preencha esse campo')
+    }else{
+        successValidation(email)
+    }
+}
+
+function errorValidation(input, message){
+    const formControl = input.parentElement;
+    const small =  formControl.querySelector('small')
+
+    small.innerText = message
+
+
+    formControl.className = 'form-control error'
+}
+
+function successValidation(input){
+    const formControl = input.parentElement;
+
+    formControl.className='form-control success'
+}
